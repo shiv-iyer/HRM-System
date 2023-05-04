54d63db71505b7889863c2d2a4231f6dff5d21fa
Shiv <shiviyer55x@gmail.com>
2023-05-04 13:50:33 +0800

    Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// Scanners allow us to ask the user for input!
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 7; i++) {
+
+            // Create a new scanner
+            // System.in essentially refers to the keyboard. in is short for input
+            Scanner sc = new Scanner(System.in);
+            float weight = sc.nextFloat();
 
             // Press Ctrl+D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Cmd+F8.
