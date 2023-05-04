bdbbbf453c11061c8d47f70bd756f4f559a59368
Shiv <shiviyer55x@gmail.com>
2023-05-04 13:55:18 +0800

    Starting work on BMI calculator

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,24 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.println("Hello world!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 7; i++) {
+        // Create a new scanner
+        // System.in essentially refers to the keyboard. in is short for input
+        Scanner sc = new Scanner(System.in);
 
-            // Create a new scanner
-            // System.in essentially refers to the keyboard. in is short for input
-            Scanner sc = new Scanner(System.in);
-            float weight = sc.nextFloat();
+        System.out.println("Please enter your weight:");
+        float weight = sc.nextFloat();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Please enter your height:");
+        float height = sc.nextFloat();
+
+        float bmi = weight / (height * height);
+
+        System.out.println("Your bmi is " + bmi);
+
     }
 }
+
+// Press Ctrl+R or click the green arrow button in the gutter to run the code.
+// Press Ctrl+D to start debugging your code. We have set one breakpoint
+// for you, but you can always add more by pressing Cmd+F8.
