57fb281fea0a97f948df4cff1a2a1b74d20d10e2
Shiv <shiviyer55x@gmail.com>
2023-05-04 14:09:34 +0800

    Employee class WIP

---
+++ src/Employee.java
@@ -0,0 +1,6 @@
+// A 'class' basically contains functions and variables
+public class Employee {
+   public String firstName;
+    public String lastName;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-// Scanners allow us to ask the user for input!
+// Scanners allow us to ask the user for input! Need to import them
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // e is a variable that will store an object of the Employee class
+        Employee e = new Employee();
+
+
+    }
+
+    public static void calculateBmi() {
         System.out.printf("Hello and welcome!");
 
         // Create a new scanner
@@ -20,10 +25,10 @@ public class Main {
         float bmi = weight / (height * height);
 
         System.out.println("Your bmi is " + bmi);
-
     }
 }
 
+// Press Opt+Enter with your caret at the highlighted text to see how
 // Press Ctrl+R or click the green arrow button in the gutter to run the code.
 // Press Ctrl+D to start debugging your code. We have set one breakpoint
 // for you, but you can always add more by pressing Cmd+F8.
